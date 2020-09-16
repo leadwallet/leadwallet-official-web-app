fe43fa0c0156f2675a3a4efc802a80128af2b94c
superdev1994 <sanchzxel@gmail.com>
2020-09-16 07:29:46 -0700

    mailchimp test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -333,6 +333,51 @@ const HomePage = () => {
               />
             </div>
           </div>
+
+          <div id="mc_embed_signup">
+            <form
+              action="https://gmail.us2.list-manage.com/subscribe/post?u=a59a82003746cc8ec56a684de&amp;id=8b078d5141"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              class="validate"
+              target="_blank"
+              novalidate
+            >
+              <div id="mc_embed_signup_scroll">
+                <label for="mce-EMAIL">Subscribe</label>
+                <input
+                  type="email"
+                  value=""
+                  name="EMAIL"
+                  class="email"
+                  id="mce-EMAIL"
+                  placeholder="email address"
+                  required
+                />
+                <div
+                  style="position: absolute; left: -5000px;"
+                  aria-hidden="true"
+                >
+                  <input
+                    type="text"
+                    name="b_a59a82003746cc8ec56a684de_8b078d5141"
+                    tabindex="-1"
+                    value=""
+                  />
+                </div>
+                <div class="clear">
+                  <input
+                    type="submit"
+                    value="Subscribe"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                    class="button"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
 
         <Footer />
