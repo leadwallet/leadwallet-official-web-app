791bbf67262beb070a7f1b2ccc11e7cdfa2c823b
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2020-10-11 22:20:53 +0100

    fix error

--- src/pages/Tokenpage.js
+++ src/pages/Tokenpage.js
@@ -27,7 +27,7 @@ const TokenPage = () => {
         <Header />
 
         <div className="container mx-auto pb-12 px-4 force-height">
-          <section className="mt-8 text-center">
+          {/* <section className="mt-8 text-center">
             <div className="Montserrat-Black uppercase text-white text-4xl mb-2">
               LEAD TOKEN
             </div>
@@ -40,66 +40,122 @@ const TokenPage = () => {
               know that LEAD token will be a cross-chain token in the future and
               won’t be just part of the Ethereum ecosystem.
             </div>
-          </section>
-
-          <section className="mt-20">
-            <Card className="px-4 md:px-12">3
-              <div className="grid grid-cols-1 sm:grid-cols-3">
-                <div className="mx-auto w-40 py-8">
-                  <img
-                    src="/images/payment.svg"
-                    width="50"
-                    alt=""
-                    className="mx-auto mb-6"
-                  />
-                  <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
-                    30% PROFIT SHARE
-                  </div>
-                </div>
-                <div className="mx-auto w-40 py-8">
-                  <img
-                    src="/images/reward.svg"
-                    width="50"
-                    alt=""
-                    className="mx-auto mb-6"
-                  />
-                  <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
-                    40% STAKE REWARD
-                  </div>
-                </div>
-                <div className="mx-auto w-40 py-8">
-                  <img
-                    src="/images/water.svg"
-                    width="50"
-                    alt=""
-                    className="mx-auto mb-6"
-                  />
-                  <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
-                    100% LIQUIDITY
-                  </div>
-                </div>
-              </div>
-            </Card>
-          </section>
+          </section> */}
 
           <section className="mt-20 text-center">
             <div className="Montserrat-Black uppercase text-white text-3xl mb-2">
               WHY LEAD TOKEN?
             </div>
             <div className="text-white w-5/6 mx-auto leading-7">
-              Many cryptocurrency projects out there do not really care where
-              the natural demand for their tokens would come from. Lead Wallet
-              and LEAD token are designed to grow together. More users for the
-              services would mean more natural demand for the LEAD token as
-              well.
+              <p className="text-white text-left">
+                Lead Token is the native currency that fuels the products and
+                services that are delivered on Lead Wallet ecosystem. Thereby
+                making Lead token a utility token as it will be needed to access
+                some major service areas of Lead Wallet application.
+              </p>
             </div>
+            <div className="text-white w-5/6 mx-auto leading-7">
+              <p className="text-white text-left">
+                The Lead Token is minted on the Ethereum VM as an ERC20 standard
+                token. There will only be a total of one billion Lead Token
+                supply.
+              </p>
+            </div>
+            <div className="text-white w-5/6 mx-auto leading-7">
+              <p className="text-white text-left">
+                LEAD's total supply is fixed. It is designed with a deflationary
+                monetary system whereby 30% of Lead Wallet quarterly profits are
+                used to purchase LEAD from the exchanges and are permanently
+                burnt.
+              </p>
+            </div>
+            <div className="text-white w-5/6 mx-auto leading-7">
+              <p className="text-white text-left">
+                Lead Wallet revenue will be generated from all the services
+                offered via the lead wallet with a quarterly audit report. Every
+                Quarter, Lead team will conduct an audit of all the earnings
+                from Lead Wallet, after which 30% of the total net profit will
+                be used to buy back a portion of the Lead token, which will be
+                permanently burnt. This process will continue until only 30% of
+                the total LEAD supply is left.
+              </p>
+            </div>
+
+            <div className="Montserrat-Black uppercase text-white text-3xl mb-2">
+              LEAD TOKEN FEATURES AND USE CASES:
+            </div>
+            <div className="text-white w-5/6 mx-auto leading-7">
+              <ul className="text-left">
+                <li className="list-disc">
+                  Up to 20% discount on purchases when you make payment with
+                  LEAD.
+                </li>
+                <li className="list-disc">
+                  Up to 180% early access APY stake rewards when you stake LEAD
+                  on Lead Wallet.
+                </li>
+                <li className="list-disc">
+                  Lead can be used as fees for several wallet transactions.
+                </li>
+                <li className="list-disc">
+                  Deflationary Monetary Supply. 30% of Lead Wallet’s quarterly
+                  profit will be used to buyback and burn LEAD token until only
+                  30% of the total supply is left.
+                </li>
+                <li className="list-disc">
+                  It can be used to trade digital gift cards and also to pay for
+                  several Lead Wallet services at a discounted rate.
+                </li>
+              </ul>
+            </div>
+
+            <section className="mt-20">
+              <Card className="px-4 md:px-12">
+                3
+                <div className="grid grid-cols-1 sm:grid-cols-3">
+                  <div className="mx-auto w-40 py-8">
+                    <img
+                      src="/images/payment.svg"
+                      width="50"
+                      alt=""
+                      className="mx-auto mb-6"
+                    />
+                    <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
+                      30% PROFIT SHARE
+                    </div>
+                  </div>
+                  <div className="mx-auto w-40 py-8">
+                    <img
+                      src="/images/reward.svg"
+                      width="50"
+                      alt=""
+                      className="mx-auto mb-6"
+                    />
+                    <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
+                      40% STAKE REWARD
+                    </div>
+                  </div>
+                  <div className="mx-auto w-40 py-8">
+                    <img
+                      src="/images/water.svg"
+                      width="50"
+                      alt=""
+                      className="mx-auto mb-6"
+                    />
+                    <div className="text-white text-center text-2xl font-Montserrat-ExtraBold uppercase">
+                      100% LIQUIDITY
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </section>
           </section>
 
           <section className="mt-20 text-center">
             <div className="Montserrat-Black uppercase text-white text-3xl mb-2">
               TOKEN DISTRIBUTION
             </div>
-            <div className="text-white w-5/6 mx-auto leading-7">
+            <div className="text-white w-5/6 mx-auto leading-7 text-left">
               The team only gets 15% of the total token distribution as we do
               not want this project to enrich the select few. We believe that
               the community deserves to get the most out of our project. Please
