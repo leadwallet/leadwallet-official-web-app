c97a2ce5a69a472f10ddbecf1e5cc9656cf60432
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2020-10-11 22:11:25 +0100

    fix error

--- main.js
+++
@@ -1,9 +0,0 @@
-const sslRedirect = require("heroku-ssl-redirect");
-const express = require("express");
-const app = express();
-// enable ssl redirect
-app.use(sslRedirect());
-app.get("/", function (req, res) {
-  res.send("hello world");
-});
-app.listen(3000);
