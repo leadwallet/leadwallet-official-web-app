9636478eb8bf79f3d4aefcaf7e1111c2434103d7
Kingsley Victor <javaprodigy56@gmail.com>
2020-11-16 10:12:47 +0100

    new commit

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 4000;
 
-const targetBaseUrl = process.env.TARGET || "https://www.leadwallet.io";
+// const targetBaseUrl = process.env.TARGET || "https://www.leadwallet.io";
 
 app.use(express.static(
  path.join(__dirname, "build")
 ));
 
 app.use(enforce.HTTPS({
- trustProtoHeader: true
+ trustProtoHeader: true,
+ trustXForwardedHostHeader: true
 }));
 
-app.get("/", (req, res) => {
- res.redirect(targetBaseUrl + "/home");
-});
+// app.get("/", (req, res) => {
+//  res.redirect(targetBaseUrl + "/home");
+// });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
