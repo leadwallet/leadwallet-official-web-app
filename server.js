0b416ce486739f0c19828f5e7daa064b676db342
Kingsley Victor <javaprodigy56@gmail.com>
2020-11-16 09:34:41 +0100

    new commit

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require("express")
+const enforce = require("express-sslify");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = process.env.PORT || 4000;
 
-app.use(express.static("build"));
+app.use(express.static(
+ path.join(__dirname, "build")
+));
+
+app.use(enforce.HTTPS({
+ trustProtoHeader: true
+}));
 
 app.get("/*", (req, res) => {
  res.sendFile(path.join(__dirname, "build", "index.html"));
